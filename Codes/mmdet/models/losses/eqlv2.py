d6082b90c3a14f695b58995ff48bbf7578a7d7f5
GL Li <452966690@qq.com>
2023-01-09 16:24:54 +0800

    Add files via upload

--- Codes/mmdet/models/losses/eqlv2.py
+++ Codes/mmdet/models/losses/eqlv2.py
@@ -2,11 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributed as dist
-from mmdet.utils import get_root_logger
 from functools import partial
-
+from .accuracy import accuracy
 from ..builder import LOSSES
 
+def get_image_count_frequency(version="v0_5"):
+    if version == "v0_5":
+        from mmdet.utils.lvis_v0_5_categories import get_image_count_frequency
+        return get_image_count_frequency()
+    elif version == "v1":
+        from mmdet.utils.lvis_v1_0_categories import get_image_count_frequency
+        return get_image_count_frequency()
+    elif version == "openimage":
+        from mmdet.utils.openimage_categories import get_instance_count
+        return get_instance_count()
+    else:
+        raise KeyError(f"version {version} is not supported")
 
 @LOSSES.register_module()
 class EQLv2(nn.Module):
@@ -22,19 +33,21 @@ class EQLv2(nn.Module):
                  vis_grad=False,
                  test_with_obj=True):
         super().__init__()
-        self.use_sigmoid = True
+        self.use_sigmoid = use_sigmoid
         self.reduction = reduction
         self.loss_weight = loss_weight
         self.class_weight = class_weight
         self.num_classes = num_classes
         self.group = True
-
+        self.custom_cls_channels = True
+        self.custom_activation = True
+        self.custom_accuracy = True
         # cfg for eqlv2
         self.vis_grad = vis_grad
         self.gamma = gamma
         self.mu = mu
         self.alpha = alpha
-
+        self.freq_info = torch.FloatTensor(get_image_count_frequency('v1'))
         # initial variables
         self.register_buffer('pos_grad', torch.zeros(self.num_classes))
         self.register_buffer('neg_grad', torch.zeros(self.num_classes))
@@ -47,8 +60,33 @@ class EQLv2(nn.Module):
         def _func(x, gamma, mu):
             return 1 / (1 + torch.exp(-gamma * (x - mu)))
         self.map_func = partial(_func, gamma=self.gamma, mu=self.mu)
-        logger = get_root_logger()
-        logger.info(f"build EQL v2, gamma: {gamma}, mu: {mu}, alpha: {alpha}")
+
+    def get_cls_channels(self, num_classes):
+        """Get custom classification channels.
+
+        Args:
+            num_classes (int): The number of classes.
+
+        Returns:
+            int: The custom classification channels.
+        """
+        assert num_classes == self.num_classes
+        return num_classes + 1
+
+    def get_accuracy(self, cls_score, labels):
+        pos_inds = labels < self.num_classes
+        acc_classes = accuracy(cls_score[pos_inds], labels[pos_inds])
+        acc = dict()
+        acc['acc_classes'] = acc_classes
+        return acc
+
+    def get_activation(self, cls_score):
+        cls_score = torch.sigmoid(cls_score)
+        n_i, n_c = cls_score.size()
+        bg_score = cls_score[:, -1].view(n_i, 1)
+        if self.test_with_obj:
+            cls_score[:, :-1] *= (1 - bg_score)
+        return cls_score
 
     def forward(self,
                 cls_score,
@@ -80,18 +118,6 @@ class EQLv2(nn.Module):
         self.collect_grad(cls_score.detach(), target.detach(), weight.detach())
 
         return self.loss_weight * cls_loss
-
-    def get_channel_num(self, num_classes):
-        num_channel = num_classes + 1
-        return num_channel
-
-    def get_activation(self, cls_score):
-        cls_score = torch.sigmoid(cls_score)
-        n_i, n_c = cls_score.size()
-        bg_score = cls_score[:, -1].view(n_i, 1)
-        if self.test_with_obj:
-            cls_score[:, :-1] *= (1 - bg_score)
-        return cls_score
 
     def collect_grad(self, cls_score, target, weight):
         prob = torch.sigmoid(cls_score)
